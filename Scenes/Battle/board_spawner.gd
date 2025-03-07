35dfe690eda7dea8cb819c1077e6349a81406114
Mark <markarneman@gmail.com>
2025-03-07 18:41:43 -0500

    auto battler netcode test

--- Scenes/Battle/board_spawner.gd
+++ Scenes/Battle/board_spawner.gd
@@ -10,9 +10,10 @@ func _ready() -> void:
 		call_deferred("spawn", 1)
 		# multiplayer.peer_connected.connect(spawn_board)
 		for peer_id: int in multiplayer.get_peers():
-			spawn(peer_id)
-			#var board = spawn(peer_id)
-			#board.set_multiplayer_authority(peer_id)
+			# spawn(peer_id)
+			var board = spawn(peer_id)
+			await board.ready
+			# board.set_multiplayer_authority(peer_id)
 			#for board_piece: Node in board.get_children():
 				#if board_piece.is_in_group("match3-pieces"):
 					#board_piece.set_multiplayer_authority(peer_id)
@@ -20,7 +21,7 @@ func _ready() -> void:
 
 func spawn_board(peer_id: int) -> Match3Board:
 	var board: Match3Board = MATCH_3_BOARD.instantiate()
-	board.set_multiplayer_authority(peer_id)
+	# board.set_multiplayer_authority(peer_id)
 	board.name = str(peer_id)
 	# board.lock()
 
--- Scripts/net_code.gd
+++ Scripts/net_code.gd
@@ -3,5 +3,5 @@ extends Node
 
 @rpc("any_peer", "call_local", "reliable")
 func remove_piece(piece: Match3Piece) -> void:
-	# piece.queue_free()
 	print("Remove Piece: %s" % piece.name)
+	piece.queue_free()
